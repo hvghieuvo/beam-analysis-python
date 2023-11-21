c5cc2f3fcc4df47fc8fe6dc64b947efe1fbf51e4
hvghieuvo <hieuvogaming@gmail.com>
2023-11-21 22:06:42 +0700

    Edit Hello.py

--- Hello.py
+++ Hello.py
@@ -26,23 +26,13 @@ def run():
 
     st.write("# Demo GUI")
 
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.header("Select above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        ### This app is a project by out team in HCMUT.
+        **👈 Select from the sidebar** to choose a tool
+        ### Check out source code in [github](https://github.com/hvghieuvo/beam-analysis-python)
     """
     )
 
