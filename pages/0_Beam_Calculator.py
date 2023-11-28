22aa4e2de792c6ed81621b83cb1a47e5d56e6446
lethanh0911 <thanh.le09112003@hcmut.edu.vn>
2023-11-28 21:21:11 +0700

    test

--- pages/0_Beam_Calculator.py
+++ pages/0_Beam_Calculator.py
@@ -2,6 +2,7 @@ from typing import Any
 import numpy as np
 import streamlit as st
 from streamlit.hello.utils import show_code
+from streamlit_option_menu ipmort option_menu 
 
 st.set_page_config(page_title='Đề 1',layout='wide')
 st.markdown("---")
@@ -10,6 +11,6 @@ st.sidebar.header('Đề 1')
 
 st.title('Tính toán dầm')
 option = st.selectbox(
-    st.write('Hãy chọn loại dầm cần tính'), 
+    'Hãy chọn loại dầm cần tính', 
     ('Dầm console','Dầm 2 gối'))
 st.write('Lựa chọn là: ', option)
