bd9d66794627a6e33493a5606ff1429baab32e52
lethanh0911 <thanh.le09112003@hcmut.edu.vn>
2023-11-28 23:37:37 +0700

    hieu test

--- pages/0_Beam_Calculator.py
+++ pages/0_Beam_Calculator.py
@@ -2,13 +2,21 @@ from typing import Any
 import numpy as np
 import streamlit as st
 from streamlit.hello.utils import show_code
-
+from streamlit_option_menu import option_menu 
 
 st.set_page_config(page_title="Beam Calculator", page_icon="🙃")
 st.markdown("# Beam Calculator")
 st.sidebar.header("Beam Calculator Tool")
 st.markdown("---")
-
-        
-
+def app():
+    st.title('Tính toán dầm')
+    selected = option_menu(None, ["Lý thuyết", "Nhập thông số đầu vào"],
+    default_index=0, orientation="horizontal",styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "20px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "blue"},
+    })
+    selected
+    
         
