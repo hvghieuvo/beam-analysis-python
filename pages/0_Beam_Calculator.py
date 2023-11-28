404ea41cabf38069a37bddd4451576b7e9fc06ea
lethanh0911 <thanh.le09112003@hcmut.edu.vn>
2023-11-29 01:25:23 +0700

    test

--- pages/0_Beam_Calculator.py
+++ pages/0_Beam_Calculator.py
@@ -10,8 +10,8 @@ st.sidebar.header("Beam Calculator Tool")
 st.markdown("---")
 tab1, tab2 = st.tabs(["Lý thuyết", "Input"])
 with tab1:
-    st.header('Theory')
-    st.text('A brief overview of the engineering theory and conventions used in this program are illustrated below. Theory is adapted from the Hibbeler textbook [2]. A more rigorous overview of the basic theory behind statically determinate structures is presented in the beambending package documentation')
+    st.header('Lý thuyết')
+    st.markdown('''<p style="font-size:20px; text-align:justyfy">A brief overview of the engineering theory and conventions used in this program are illustrated below. Theory is adapted from the Hibbeler textbook [2]. A more rigorous overview of the basic theory behind statically determinate structures is presented in the beambending package documentation.</p>''',unsafe_allow_html=True)
     st.link_button('Ấn để biết thêm chi tiết','https://indeterminatebeam.readthedocs.io/en/main/theory.html?fbclid=IwAR18lJpYVJm1MnqkVdXydhA0eLWQwSmCV4w6VzKAIK5dueK9zq-_gYrxMy0')
 
 
