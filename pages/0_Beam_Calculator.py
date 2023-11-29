c29ef04891000d84ccf0707482286b049aedbbb6
lethanh0911 <thanh.le09112003@hcmut.edu.vn>
2023-11-29 22:13:04 +0700

    test

--- pages/0_Beam_Calculator.py
+++ pages/0_Beam_Calculator.py
@@ -19,18 +19,19 @@ with tab2:
     col1, col2 = st.columns(2)
     with col1:
         st.subheader('Chọn loại dầm')
-        select = st.selectbox('Chọn loại dầm',
-        ('Dầm console', 'Dầm 2 gối'))
-        if select == 'Dầm console':
-            length = st.number_input('Length of beam?',min_value=0,max_value=None,step=1, placeholder='Type a number...')
-            num_forces = st.number_input('Amount of force want to add?', min_value=0,max_value=None,step=1, placeholder='Type a number...')
-            if num_forces > 0:
-                st.write(f"Equivalent forces:")
-            for i in range(1, num_forces + 1):
-                force_position = st.number_input(f'Force {i} - Vị trí lực:', min_value=0, max_value=length, step=1, key=f'position_{i}')
-                force_magnitude = st.number_input(f'Force {i} - Độ lớn lực:', min_value=0, max_value=None, step=1, key=f'magnitude_{i}')
+        def main():
+            select = st.selectbox('Chọn loại dầm',
+            ('Dầm console', 'Dầm 2 gối'))
+            if select == 'Dầm console':
+                length = st.number_input('Length of beam?',min_value=0,max_value=None,step=1, placeholder='Type a number...')
+                num_forces = st.number_input('Amount of force want to add?', min_value=0,max_value=None,step=1, placeholder='Type a number...')
+                if num_forces > 0:
+                    st.write(f"Equivalent forces:")
+                for i in range(1, num_forces + 1):
+                    force_position = st.number_input(f'Force {i} - Vị trí lực:', min_value=0, max_value=length, step=1, key=f'position_{i}')
+                    force_magnitude = st.number_input(f'Force {i} - Độ lớn lực:', min_value=0, max_value=None, step=1, key=f'magnitude_{i}')
 
-                st.write(f"Force {i}: Position - {force_position}, Magnitude - {force_magnitude}")
+                    st.write(f"Force {i}: Position - {force_position}, Magnitude - {force_magnitude}")
 
             if __name__ == '__main__':
                 main()
